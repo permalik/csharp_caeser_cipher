ad0a786cdb9839b5dcbb16406ff42d174f06d9d1
permalik <permalik@protonmail.com>
2021-12-16 12:39:09 -0600

    Complete Encryption Decryption programs

--- DecryptCipher/DecryptCipher.cs
+++ DecryptCipher/DecryptCipher.cs
@@ -6,32 +6,75 @@ public class DecryptCipher
 {
     public static StringBuilder Decrypt(String text, int shift)
     {
-        StringBuilder result = new StringBuilder();
+        shift %= 26;
+        StringBuilder singleWordResult = new StringBuilder();
+        StringBuilder multiWordResult = new StringBuilder();
+        char[] delimiters = {' ', '\r', '\n', '\t', '.', ',', ';', ':'};
 
-        foreach (var t in text)
+        if (text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Length == 1)
         {
-            if (char.IsUpper(t))
+            for (int i = 0; i < text.Length; i++)
             {
-                char character = (char) ((t - shift - 65) % 26 + 65);
-                result.Append(character);
+                if (char.IsUpper(text[i]))
+                {
+                    char character = (char) ((text[i] + shift - 65) % 26 + 65);
+                    singleWordResult.Append(character);
+                }
+                else
+                {
+                    char character = (char) ((text[i] + shift - 97) % 26 + 97);
+                    singleWordResult.Append(character);
+                }   
             }
-            else
-            {
-                char character = (char) ((t - shift - 97) % 26 + 97);
-                result.Append(character);
-            }
+            return singleWordResult;   
         }
-        return result;
+
+        if (text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Length > 1)
+        {
+            String[] textArray = text.Split(delimiters);
+
+            for (int i = 0; i < textArray.Length; i++)
+            {
+                singleWordResult.Clear();
+                
+                for (int n = 0; n < textArray[i].Length; n++)
+                {
+                    if (char.IsUpper(textArray[i][n]))
+                    {
+                        char character = (char) ((textArray[i][n] + shift - 65) % 26 + 65);
+                        singleWordResult.Append(character);
+                    }
+                    else
+                    {
+                        char character = (char) ((textArray[i][n] + shift - 97) % 26 + 97);
+                        singleWordResult.Append(character);
+                    }   
+                }
+
+                String currentString = singleWordResult.ToString();
+
+                if (i == textArray.Length - 1)
+                {
+                    multiWordResult.Append(currentString);
+                }
+                else
+                {
+                    multiWordResult.Append(currentString).Append(' ');
+                }
+            }
+            return multiWordResult;
+        }
+        return null!;
     }
     // Driver
     public static void Main(String[] args)
     {
-        Console.WriteLine("Input text to decrypt: ");
+        Console.WriteLine("Input text for decryption: ");
         String text = Console.ReadLine();
         Console.WriteLine("Input shift for decryption: ");
         int shift = Int32.Parse(Console.ReadLine());
         Console.WriteLine("Text: " + text);
         Console.WriteLine("Shift: " + shift);
-        Console.WriteLine("Cipher: " + Decrypt(text, shift));
+        Console.WriteLine("Cipher: " + Decrypt(text,26 - shift));
     }
 }
--- EncryptCipher/EncryptCipher.cs
+++ EncryptCipher/EncryptCipher.cs
@@ -6,28 +6,72 @@ public class EncryptCipher
 {
      public static StringBuilder Encrypt(String text, int shift)
      {
-          StringBuilder result = new StringBuilder();
-
-          foreach (var t in text)
+          StringBuilder singleWordResult = new StringBuilder();
+          StringBuilder multiWordResult = new StringBuilder();
+          char[] delimiters = {' ', '\r', '\n', '\t', '.', ',', ';', ':'};
+          
+          if (text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Length == 1)
           {
-               if (char.IsUpper(t))
+               for (int i = 0; i < text.Length; i++)
                {
-                    char character = (char) ((t + shift - 65) % 26 + 65);
-                    result.Append(character);
+                    if (char.IsUpper(text[i]))
+                    {
+                         char character = (char) ((text[i] + shift - 65) % 26 + 65);
+                         singleWordResult.Append(character);
+                    }
+                    else
+                    {
+                         char character = (char) ((text[i] + shift - 97) % 26 + 97);
+                         singleWordResult.Append(character);
+                    }    
                }
-               else
-               {
-                    char character = (char) ((t + shift - 97) % 26 + 97);
-                    result.Append(character);
-               }
+               return singleWordResult;
           }
-          return result;
+
+          if (text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Length > 1)
+          {
+               String[] textArray = text.Split(delimiters);
+
+               for (int i = 0; i < textArray.Length; i++)
+               {
+                    singleWordResult.Clear();
+
+                    for (int n = 0; n < textArray[i].Length; n++)
+                    {
+                         if (char.IsUpper(textArray[i][n]))
+                         {
+                              char character = (char) ((textArray[i][n] + shift - 65) % 26 + 65);
+                              singleWordResult.Append(character);
+                         }
+                         else
+                         {
+                              char character = (char) ((textArray[i][n] + shift - 97) % 26 + 97);
+                              singleWordResult.Append(character);
+                         }
+                    }
+
+                    String currentString = singleWordResult.ToString();
+                    
+                    if (i == textArray.Length - 1)
+                    {
+                         multiWordResult.Append(currentString);
+                    }
+                    else
+                    {
+                         multiWordResult.Append(currentString).Append(' ');
+                    }
+               }
+               return multiWordResult;
+          }
+          return null!;
      }
      // Driver
      public static void Main(String[] args)
      {
-          String text = "Caeser Cipher";
-          int shift = 5;
+          Console.WriteLine("Input text for encryption: ");
+          String text = Console.ReadLine();
+          Console.WriteLine("Input shift for encryption: ");
+          int shift = Int32.Parse(Console.ReadLine());
           Console.WriteLine("Text: " + text);
           Console.WriteLine("Shift: " + shift);
           Console.WriteLine("Cipher: " + Encrypt(text, shift));
